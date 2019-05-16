a86ed5ae783a7aa0c8c112bdf201ab1403e8269b
Min RK <benjaminrk@gmail.com>
2019-05-16 15:04:54 +0200

    debug: test absolute import (this is what segfaults)

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,7 +1,12 @@
+set -ex
+
 nrnivmodl
 ./x86_64/special --version
 
 python -c "import neuron; neuron.test()"
 python -c "import neuron; neuron.h.load_file('stdlib.hoc')"
 
-conda env export
+conda env export -p $CONDA_PREFIX
+
+python -c "import neuron; neuron.h.load_file(neuron.h.neuronhome() + '/stdlib.hoc')"
+
