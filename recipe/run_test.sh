273b88d79a0bede27ab6d4a723b693c6fc08a35d
Min RK <benjaminrk@gmail.com>
2019-05-16 19:32:08 +0200

    asserts in tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,10 +3,9 @@ set -ex
 nrnivmodl
 ./x86_64/special --version
 
-python -c "import neuron; neuron.test()"
-python -c "import neuron; neuron.h.load_file('stdlib.hoc')"
-
 conda env export -p $CONDA_PREFIX
 
-python -c "import neuron; neuron.h.load_file(neuron.h.neuronhome() + '/stdlib.hoc')"
+python -c "import neuron; neuron.test()"
+python -c "import neuron; neuron.h.load_file('stdlib.hoc')"
+python -c "import neuron; neuron.h.load_file(neuron.h.neuronhome() + '/lib/hoc/stdlib.hoc')"
 
