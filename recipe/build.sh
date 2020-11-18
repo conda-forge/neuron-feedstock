5e8f5d6ec2417c161d8d06e4f95544b8f0360b7c
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-11-18 23:16:42 +0100

    comment out exports

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,27 +2,27 @@
 #cp -fR yale/* github/
 #cd github
 
-'''
-# cancel culling of unused libs
-# it seems to cull libs that are actually used
-export LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs}"
-export LDFLAGS="${LDFLAGS/-Wl,--as-needed}"
 
-# force shortnames of compilers since package contains references to these
-if [[ "$(uname)" == "Darwin" ]]; then
-  export CC=clang
-  export CXX=clang++
-  # LDSHARED needed for Python (mac only, apparently)
-  export LDSHARED="${LD:-$CXX} -bundle -undefined dynamic_lookup $LDFLAGS"
-else
-  export CC=$(basename $CC)
-  export CXX=$(basename $CXX)
-  # clear C++ compiler flags, which have been identified
-  # as the culprit
-  export CPPFLAGS="-I$PREFIX/include"
-  export CXXFLAGS="-fPIC -I$PREFIX/include"
-fi
-'''
+# # cancel culling of unused libs
+# # it seems to cull libs that are actually used
+# export LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs}"
+# export LDFLAGS="${LDFLAGS/-Wl,--as-needed}"
+
+# # force shortnames of compilers since package contains references to these
+# if [[ "$(uname)" == "Darwin" ]]; then
+#   export CC=clang
+#   export CXX=clang++
+#   # LDSHARED needed for Python (mac only, apparently)
+#   export LDSHARED="${LD:-$CXX} -bundle -undefined dynamic_lookup $LDFLAGS"
+# else
+#   export CC=$(basename $CC)
+#   export CXX=$(basename $CXX)
+#   # clear C++ compiler flags, which have been identified
+#   # as the culprit
+#   export CPPFLAGS="-I$PREFIX/include"
+#   export CXXFLAGS="-fPIC -I$PREFIX/include"
+# fi
+
 
 # force cython recompile by removing cython-generated sources
 find share/lib/python -name "*.cpp" -exec rm {} \;
