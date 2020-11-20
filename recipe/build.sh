228fd8e386c584ab75f2980f390280d27df6b6d3
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-11-20 23:16:17 +0100

    linux build ran without crashing locally

--- recipe/build.sh
+++ recipe/build.sh
@@ -55,7 +55,7 @@
 # # since package installs in lib/python instead of proper site-packages
 # cd src/nrnpython
 # $PREFIX/bin/python setup.py install
-# rm -rf $PREFIX/lib/python/neuron
+# rm -rf $PREFIX/lib/python/neurons
 # rm -f $PREFIX/lib/python/NEURON-*
 # rmdir $PREFIX/lib/python || true
 # # rm -rf $PREFIX/share/nrn/lib/python
@@ -67,21 +67,19 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "3.8" ]; then
-        pip install https://files.pythonhosted.org/packages/3a/8a/ca66dc5cf5f49237ae2c854b4f5b179786a8e55c64aac4a757a2a13f3193/NEURON-7.8.1.1-cp38-cp38-macosx_10_9_x86_64.whl --no-deps -v
+        pip install https://files.pythonhosted.org/packages/3a/8a/ca66dc5cf5f49237ae2c854b4f5b179786a8e55c64aac4a757a2a13f3193/NEURON-7.8.1.1-cp38-cp38-macosx_10_9_x86_64.whl --no-deps --ignore-installed --no-cache-dir -vvv
     else
-        pip install NEURON --no-deps -v
+        pip install NEURON --no-deps -vs
     fi
 fi
 
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "3.8" ]; then
-        pip install https://files.pythonhosted.org/packages/ef/73/2bda5b486fda5a8e437331bba76b431fd58c9cea5ff80e73e64646b45523/NEURON-7.8.1.1-cp38-cp38-manylinux1_x86_64.whl --no-deps -v
+        pip install https://files.pythonhosted.org/packages/ef/73/2bda5b486fda5a8e437331bba76b431fd58c9cea5ff80e73e64646b45523/NEURON-7.8.1.1-cp38-cp38-manylinux1_x86_64.whl --no-deps --ignore-installed --no-cache-dir -vvv
     else
         pip install NEURON --no-deps -v
     fi
 fi
-
-
 
 # test imports
 python -c 'import neuron.hoc'
