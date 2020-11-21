07240e1c460703210ef5009289e7cb24baa24f1b
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-11-21 10:54:43 +0100

    run mpi test script with openMPI

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,3 +8,13 @@ conda env export -p $CONDA_PREFIX
 python -c "import neuron; neuron.test()"
 python -c "import neuron; assert neuron.h.load_file(neuron.h.neuronhome() + '/lib/hoc/stdlib.hoc')"
 python -c "import neuron; assert neuron.h.load_file('stdlib.hoc')"
+
+
+
+# test with OpenMPI
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_rmaps_base_oversubscribe=yes
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+mpiexec="mpiexec --allow-run-as-root"
+
+$mpiexec -n 3 python test_mpi.py 2>&1 | cat
